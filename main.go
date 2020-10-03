d6bbda411e978d826751591cfb2dd5574942ec97
Mason <mason.cao@uwaterloo.ca>
2020-10-03 14:41:46 -0400

    add msg for when          server starts

--- main.go
+++ main.go
@@ -24,5 +24,8 @@ func main() {
 	// database.AutoMigrateDB()
 	r := mux.NewRouter()
 	api.ResgisterRoutes(r)
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", r))
+	log.Println("Server started, running on port 8080.")
+	if err := http.ListenAndServe("127.0.0.1:8080", r); err != nil {
+		log.Fatal("Server failed to start: ", err.Error())
+	}
 }
