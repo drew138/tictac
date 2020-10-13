1719e58f7e3b7517be5816100097e86bd74fb0c2
drew <andresalazar138@gmail.com>
2020-10-12 20:56:19 -0500

    removed onUpdate constraint in games struct

--- database/models/games.go
+++ database/models/games.go
@@ -4,8 +4,8 @@ import "time"
 
 // Game - Information pertaining searches done by each user
 type Game struct {
-	ID         int  `gorm:"primaryKey"`
-	PlayersOne User `gorm:"foreignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	PlayersTwo User `gorm:"foreignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ID         uint `gorm:"primaryKey"`
+	PlayersOne User `gorm:"foreignKey:ID;constraint:OnDelete:SET NULL;"`
+	PlayersTwo User `gorm:"foreignKey:ID;constraint:OnDelete:SET NULL;"`
 	CreatedAt  time.Time
 }
