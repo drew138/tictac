95c34e339e744cf7804ee573382b30047c1f72bb
Mason <mason.cao@uwaterloo.ca>
2020-11-24 12:29:20 -0500

    add private messaging

--- api/websockets/connections/connections.go
+++ api/websockets/connections/connections.go
@@ -3,6 +3,7 @@ package connections
 import (
 	"log"
 
+	messages "github.com/drew138/tictac/api/websockets/messages/tictactoe"
 	"github.com/gorilla/websocket"
 )
 
@@ -15,8 +16,9 @@ type Connections struct {
 
 // ConnectedUser holds information of a connected user, as well as their websocket connection
 type ConnectedUser struct {
-	UserID string
-	Conn   *websocket.Conn
+	UserID    string
+	Conn      *websocket.Conn
+	SendQueue chan *messages.Message
 }
 
 // StartConnectionTracking starts connection workers and returns Connections struct
@@ -39,8 +41,14 @@ func (c *Connections) startConnectionsWorker() {
 			break
 		case user := <-c.Disconnect:
 			delete(c.connectedUsers, user.UserID)
+			close(user.SendQueue)
 			log.Println("Removed user", user.UserID, "from connection pool")
 			break
 		}
 	}
+}
+
+// GetConnectedUser returns the conected user
+func (c *Connections) GetConnectedUser(id string) *ConnectedUser {
+	return c.connectedUsers[id]
 }
--- api/websockets/messages/generic.go
+++
@@ -1,6 +0,0 @@
-package messages
-
-// Message is main format for messages
-type Message struct {
-	Action string `json:"action"`
-}
